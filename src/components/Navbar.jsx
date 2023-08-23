258234a4c4aecad7006e0d0e37e3677613ee9980
Lucas <lumuryllo86@gmail.com>
2023-08-23 15:10:22 -0300

    :construction: Does the Ecommerce Home page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,126 +10,109 @@ import avatar from '../data/avatar.jpg';
 import { Cart, Chat, Notification, UserProfile } from '.';
 import { useStateContext } from '../context/ContextProvider';
 
-const NavButton = ({
-  title,
-  custonFunc,
-  icon,
-  color,
-  dotColor }) => (
+const NavButton = ({ title, customFunc, icon, color, dotColor }) => (
   <TooltipComponent
     content={title}
-    position='BottomCenter'>
+    position="BottomCenter">
     <button
-      type='button'
-      onClick={custonFunc}
+      type="button"
+      onClick={() => customFunc()}
       style={{ color }}
-      className='relative text-xl rounded-full p-3  hover:bg-light-gray'
+      className="relative text-xl rounded-full p-3 hover:bg-light-gray"
     >
       <span
         style={{ background: dotColor }}
-        className='absolute inline-flex rounded-full h-2 w-2 right-2 top-2' 
+        className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
       />
       {icon}
     </button>
-
   </TooltipComponent>
-)
-
+);
 
 const Navbar = () => {
-  const {
+  const { currentColor,
     activeMenu,
     setActiveMenu,
-    isClicked,
-    setIsClicked,
     handleClick,
-    screenSize,
-    setScreenSize
-  } = useStateContext()
+    isClicked,
+    setScreenSize,
+    screenSize } = useStateContext();
 
   useEffect(() => {
-    const handleResize = () => setScreenSize(window.innerWidth)
+    const handleResize = () => setScreenSize(window.innerWidth);
 
-    window.addEventListener('resize', handleResize)
-    handleResize()
+    window.addEventListener('resize', handleResize);
 
-    return () => window.removeEventListener('resize', handleResize)
-  }, [])
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
   useEffect(() => {
     if (screenSize <= 900) {
-      setActiveMenu(false)
+      setActiveMenu(false);
     } else {
-      setActiveMenu(true)
+      setActiveMenu(true);
     }
-  }, [screenSize])
+  }, [screenSize]);
 
+  const handleActiveMenu = () => setActiveMenu(!activeMenu);
 
   return (
-    <div className='flex justify-between p-2 md:mx-6 relative'>
-      <NavButton
-        title='Menu'
-        custonFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
-        color='blue'
-        icon={<AiOutlineMenu />}
-      />
+    <div className="flex justify-between p-2 md:ml-6 md:mr-6 relative">
 
-      <div className='flex'>
+      <NavButton title="Menu" customFunc={handleActiveMenu} color={currentColor} icon={<AiOutlineMenu />} />
+      <div className="flex">
         <NavButton
-          title='Cart'
-          // eslint-disable-next-line no-undef
-          custonFunc={() => handleClick('cart')}
-          color='blue'
+          title="Cart"
+          customFunc={() => handleClick('cart')}
+          color={currentColor}
           icon={<FiShoppingCart />}
         />
         <NavButton
-          title='Chat'
-          dotColor='#03C9D7'
-          // eslint-disable-next-line no-undef
-          custonFunc={() => handleClick('chat')}
-          color='blue'
+          title="Chat"
+          dotColor="#03C9D7"
+          customFunc={() => handleClick('chat')}
+          color={currentColor}
           icon={<BsChatLeft />}
         />
         <NavButton
-          title='Notifications'
-          dotColor='#03C9D7'
-          // eslint-disable-next-line no-undef
-          custonFunc={() => handleClick('notification')}
-          color='blue'
+          title="Notification"
+          dotColor="rgb(254, 201, 15)"
+          customFunc={() => handleClick('notification')}
+          color={currentColor}
           icon={<RiNotification3Line />}
         />
         <TooltipComponent
-          content='profile'
-          position='BottomCenter'
+          content="Profile"
+          position="BottomCenter"
         >
           <div
-            className='flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg'
-            // eslint-disable-next-line no-undef
+            className="flex items-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
             onClick={() => handleClick('userProfile')}
           >
             <img
-              className='rounded-full w-8 h-8'
+              className="rounded-full w-8 h-8"
               src={avatar}
+              alt="user-profile"
             />
             <p>
-              <span className='text-gray-400 text-14'>Hi, </span> {' '}
-              <span className='text-gray-400 font-bold ml-1 text-14'>Michael</span>
+              <span className="text-gray-400 text-14">Hi,</span>{' '}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Michael
+              </span>
             </p>
-            <MdKeyboardArrowDown
-              className='text-gray-400 text-14'
-            />
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
           </div>
-
         </TooltipComponent>
 
-        {isClicked.cart && <Cart />}
-        {isClicked.chat && <Chat />}
-        {isClicked.notification && <Notification />}
-        {isClicked.userProfile && <UserProfile />}
-
+        {isClicked.cart && (<Cart />)}
+        {isClicked.chat && (<Chat />)}
+        {isClicked.notification && (<Notification />)}
+        {isClicked.userProfile && (<UserProfile />)}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
