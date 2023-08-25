263cc3a1b9ac280da02a5b3da1cc8acda1383d2d
Lucas <lumuryllo86@gmail.com>
2023-08-25 12:28:50 -0300

    :ok_hand: Conclued Charts

--- src/pages/Charts/Line.jsx
+++ src/pages/Charts/Line.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
 
-const Line = () => {
-  return (
-    <div>Line</div>
-  )
-}
+import { ChartsHeader, LineCharts } from '../../components';
 
-export default Line
+const Line = () => (
+  <div className="m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+    <ChartsHeader
+      category="Line"
+      title="Inflation Rate"
+    />
+    <div className="w-full">
+      <LineCharts />
+    </div>
+  </div>
+);
+
+export default Line;
